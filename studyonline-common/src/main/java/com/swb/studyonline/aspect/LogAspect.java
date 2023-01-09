ec90567dcb51e4da348d98679376d3e20a472190
swb <climax_sun@163.com>
2023-01-09 11:20:00 +0800

    版本调整

--- studyonline-common/src/main/java/com/swb/studyonline/aspect/LogAspect.java
+++ studyonline-common/src/main/java/com/swb/studyonline/aspect/LogAspect.java
@@ -7,6 +7,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -23,6 +24,7 @@ import java.util.stream.Stream;
  *
  * @author swb
  */
+@ConditionalOnExpression("${logging.accessLogEnable:false}")
 @Aspect
 @Component
 public class LogAspect {
